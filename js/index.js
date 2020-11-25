67e6c8f359a7bdea66c339ecdcd2817a0de005bd
Dylan Morison <dylan@g.morison.org>
2020-11-24 17:18:05 -0800

    lots of progress made more then half way done!

--- js/index.js
+++ js/index.js
@@ -6,10 +6,76 @@ $(document).ready(function () {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Software Engineer", "Full-Stack Web Developer", "Udemy Fanatic", "Javascript Lover"],
+		strings: [
+			"Software Engineer",
+			"Full-Stack Web Developer",
+			"Udemy Fanatic",
+			"Javascript Lover"
+		],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
 		showCursor: false
 	});
+
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		items: 4,
+		responsive: {
+			0: {
+				items: 1
+			},
+			400: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+			938: {
+				items: 4
+			}
+		}
+	});
+
+	var skillsTopOffset = $(".skillsSection").offset().top;
+	var statsTopOffset = $(".statsSection").offset().top;
+	var countUpFinished = false;
+
+	$(window).scroll(function () {
+		if (window.pageYOffset > skillsTopOffset - $(window).height() + 250) {
+			$(".chart").easyPieChart({
+				autoplay: true,
+				autoplayTimeout: 5000,
+				nav: true,
+				easing: "easeInOut",
+				barColor: "#fff",
+				trackColor: false,
+				scaleColor: false,
+				lineWidth: 4,
+				size: 152,
+				onStep: function (from, to, percent) {
+					$(this.el).find(".percent").text(Math.round(percent));
+				}
+			});
+			var owl = $(".owl-carousel");
+
+			owl.trigger("play.owl.autoplay", [3000]);
+		}
+
+		if (
+			!countUpFinished &&
+			window.pageYOffset > statsTopOffset - $(window).height() + 250
+		) {
+			$(".counter").each(function () {
+				var element = $(this);
+				var endVal = parseInt(element.text());
+
+				element.countup(endVal);
+			});
+
+			countUpFinished = true;
+		}
+	});
+
+	$("[data-fancybox]").fancybox();
 });
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(express.static("public"));
 app.use(express.static("css"));
 app.use(express.static("js"));
 
+
 app.all("/", (req, res) => {
 	res.sendFile(path.join(__dirname + "/index.html"));
 });
