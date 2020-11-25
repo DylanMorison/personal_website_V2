329d9cedaacfa07d5a27db9be6812cb32e47722c
Dylan Morison <dylan@g.morison.org>
2020-11-24 21:21:00 -0800

    finished!

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+$(window).on("load", function () {
+	$(".loader .inner").fadeOut(500, function () {
+		$(".loader").fadeOut(750);
+	});
+
+	$(".items").isotope({
+		filter: "*",
+		animationOptions: {
+			duration: 1500,
+			easing: "linear",
+			queue: false
+		}
+	});
+});
+
 $(document).ready(function () {
 	$("#slides").superslides({
 		animation: "fade",
@@ -32,7 +47,7 @@ $(document).ready(function () {
 				items: 3
 			},
 			938: {
-				items: 4
+				items: 5
 			}
 		}
 	});
@@ -78,4 +93,39 @@ $(document).ready(function () {
 	});
 
 	$("[data-fancybox]").fancybox();
+
+	$("#filters a").click(function () {
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: "linear",
+				queue: false
+			}
+		});
+
+		return false;
+	});
+
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+		var body = $("body");
+
+		if ($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");
+		} else {
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav");
+		}
+	}
 });
